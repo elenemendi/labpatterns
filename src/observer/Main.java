d90a2aab6dede2c1dd12e134432a492bd6e9dab6
Elene MH <emendizabal013@ikasle.ehu.eus>
2020-11-03 13:05:13 +0100

    Observer implementazioa

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -10,7 +10,25 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-
+		Covid19Pacient pacient = new Covid19Pacient("aitor", 35);
+		Covid19Pacient pacient2 = new Covid19Pacient("elene", 82);
+		Covid19Pacient pacient3 = new Covid19Pacient("igor", 46);
+		
+		PacientObserverGUI pacientGUI = new PacientObserverGUI(pacient);
+		PacientObserverGUI pacientGUI2 = new PacientObserverGUI(pacient2);
+		PacientObserverGUI pacientGUI3 = new PacientObserverGUI(pacient3);
+		
+		PacientSymptomGUI frame = new PacientSymptomGUI(pacient);
+		PacientSymptomGUI frame2 = new PacientSymptomGUI(pacient2);
+		PacientSymptomGUI frame3 = new PacientSymptomGUI(pacient3);
+		
+		PacientThermometerGUI ter = new PacientThermometerGUI(pacient);
+		PacientThermometerGUI ter2 = new PacientThermometerGUI(pacient2);
+		PacientThermometerGUI ter3 = new PacientThermometerGUI(pacient3);
+		
+		SemaphorGUI sem = new SemaphorGUI(pacient);
+		SemaphorGUI sem2 = new SemaphorGUI(pacient2);
+		SemaphorGUI sem3 = new SemaphorGUI(pacient3);
 	}
 
 
--- src/observer/SemaphorGUI.java
+++ src/observer/SemaphorGUI.java
@@ -1,22 +1,48 @@
 package observer;
 
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JFrame;
 
+import domain.Covid19Pacient;
 
-public class SemaphorGUI extends JFrame {
+
+public class SemaphorGUI extends JFrame implements Observer {
 	/** stores the associated ConcreteSubject */
-	public SemaphorGUI () {
+	public SemaphorGUI (Observable obs) {
 		setSize(100, 100);
 		setLocation(350,10);
 		Color c=Color.green;
 		getContentPane().setBackground(c);
 		repaint();
 		setVisible(true);
+		obs.addObserver(this);
 	}
+	
+	public void update(Observable obs, Object o) {
+		Covid19Pacient p=(Covid19Pacient)obs;
+		Color c;
+		// Obtain the  current covidImpact to paint
+		int farenheit= (int) p.covidImpact(); 
+		// temperature gauge update
+		if (farenheit<5) {
+			c=Color.green;
+		}else if(farenheit>10) {
+			c=Color.red;
+		}else {
+			c=Color.yellow;
+		}
+		
+		getContentPane().setBackground(c);
+		
+	}
+	
+		
+
 }     
 
